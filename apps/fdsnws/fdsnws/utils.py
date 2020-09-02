c04a087d69381dd78fc7b2b169b551467c2160bf
Andres Heinloo <andres@gfz-potsdam.de>
2020-09-02 20:15:00 +0200

    [fdsnws] avoid crash (Swig::DirectorMethodException) with invalid unicode

--- apps/fdsnws/fdsnws/utils.py
+++ apps/fdsnws/fdsnws/utils.py
@@ -23,7 +23,7 @@ from seiscomp.client import Application
 
 #-------------------------------------------------------------------------------
 # Converts a unicode string to a byte string
-b_str = lambda s: s.encode('utf-8')
+b_str = lambda s: s.encode('utf-8', 'replace')
 
 
 #-------------------------------------------------------------------------------
