223ec91f21a6d5cbb79b3d14615d0c6a92f3dbb6
Dirk Roessler <roessler@gempa.de>
2023-09-09 22:01:57 +0200

    [scorgls] Add -i option, support mutiple time formats

--- apps/python/descriptions/scorgls.rst
+++ apps/python/descriptions/scorgls.rst
@@ -1,17 +1,24 @@
 *scorgls* lists all available origin IDs within a given time range to stdout.
+Origins are fetched from database or read from a :term:`SCML` file.
 
-Use :ref:`scevtls` for listing all event IDs. In extension to :ref:`scevtls`
-and scorgls :ref:`scquery` can search for parameters based on complex custom
-queries.
+Similarly, use :ref:`scevtls` for listing all event IDs. In extension to
+*scorgls* and :ref:`scevtls` :ref:`scquery` can search for parameters based on
+complex custom queries.
 
 
-Example
-=======
+Examples
+========
 
-Print all origin IDs for the complete year 2012.
+* Print all origin IDs for the complete year 2012.
 
-.. code-block:: sh
+  .. code-block:: sh
 
-   scorgls -d mysql://sysop:sysop@localhost/seiscomp \
-           --begin "2012-01-01 00:00:00" \
-           --end "2013-01-01 00:00:00"
+     scorgls -d mysql://sysop:sysop@localhost/seiscomp \
+             --begin "2012-01-01 00:00:00" \
+             --end "2013-01-01 00:00:00"
+
+* Print the IDs of all origins provided with the XML file:
+
+  .. code-block:: sh
+
+     scevtls -i origins.xml
--- apps/python/scorgls.py
+++ apps/python/scorgls.py
@@ -13,10 +13,68 @@
 # https://www.gnu.org/licenses/agpl-3.0.html.                              #
 ############################################################################
 
+import os
 import sys
+
 import seiscomp.core
 import seiscomp.client
 import seiscomp.datamodel
+
+
+def _parseTime(timestring):
+    t = seiscomp.core.Time()
+    if t.fromString(timestring, "%F %T"):
+        return t
+    if t.fromString(timestring, "%FT%T"):
+        return t
+    if t.fromString(timestring, "%FT%TZ"):
+        return t
+    if t.fromString(timestring, "%F"):
+        return t
+    return None
+
+
+def readXML(self):
+    ar = seiscomp.io.XMLArchive()
+    if not ar.open(self._inputFile):
+        print(f"Unable to open input file {self._inputFile}")
+        return []
+
+    obj = ar.readObject()
+    if obj is None:
+        raise TypeError("invalid input file format")
+
+    ep = seiscomp.datamodel.EventParameters.Cast(obj)
+    if ep is None:
+        raise ValueError("no event parameters found in input file")
+
+    originIDs = []
+    for i in range(ep.originCount()):
+        org = ep.origin(i)
+
+        # check time requirements
+        orgTime = org.time().value()
+        if orgTime < self._startTime:
+            continue
+        if orgTime > self._endTime:
+            continue
+
+        # check author requirements
+        if self.author:
+            try:
+                author = org.creationInfo().author()
+            except:
+                continue
+
+            if author != self.author:
+                continue
+
+        try:
+            originIDs.append(org.publicID())
+        except:
+            continue
+
+    return originIDs
 
 
 class OriginList(seiscomp.client.Application):
@@ -30,8 +88,16 @@ class OriginList(seiscomp.client.Application):
         self._startTime = seiscomp.core.Time()
         self._endTime = seiscomp.core.Time.GMT()
         self._delimiter = None
+        self._inputFile = None
 
     def createCommandLineDescription(self):
+        self.commandline().addGroup("Input")
+        self.commandline().addStringOption(
+            "Input",
+            "input,i",
+            "Name of input XML file. Read from stdin if '-' is given. Deactivates "
+            "reading events from database",
+        )
         self.commandline().addGroup("Origins")
         self.commandline().addStringOption(
             "Origins",
@@ -55,30 +121,49 @@ class OriginList(seiscomp.client.Application):
         )
         return True
 
+    def validateParameters(self):
+        if not seiscomp.client.Application.validateParameters(self):
+            return False
+
+        try:
+            self._inputFile = self.commandline().optionString("input")
+        except RuntimeError:
+            pass
+
+        if self._inputFile:
+            self.setDatabaseEnabled(False, False)
+
+        return True
+
     def init(self):
         if not seiscomp.client.Application.init(self):
             return False
 
         try:
             start = self.commandline().optionString("begin")
-            if not self._startTime.fromString(start, "%F %T"):
-                print(
-                    f"Wrong 'begin' given -> assuming {self._startTime}",
-                    file=sys.stderr,
-                )
         except RuntimeError:
-            print(f"No 'begin' given -> assuming {self._startTime}", file=sys.stderr)
+            start = "1900-01-01T00:00:00Z"
+
+        self._startTime = _parseTime(start)
+        if self._startTime is None:
+            seiscomp.logging.error(f"Wrong 'begin' format '{start}'")
+            return False
+        seiscomp.logging.debug(f"Setting start to {self._startTime.toString('%FT%TZ')}")
 
         try:
             end = self.commandline().optionString("end")
-            if not self._endTime.fromString(end, "%F %T"):
-                print(f"Wrong 'end' given -> assuming {self._endTime}", file=sys.stderr)
         except RuntimeError:
-            print(f"No 'end' given -> assuming {self._endTime}", file=sys.stderr)
+            end = "2500-01-01T00:00:00Z"
+
+        self._endTime = _parseTime(end)
+        if self._endTime is None:
+            seiscomp.logging.error(f"Wrong 'end' format '{end}'")
+            return False
+        seiscomp.logging.debug(f"Setting end to {self._endTime.toString('%FT%TZ')}")
 
         try:
             self.author = self.commandline().optionString("author")
-            sys.stderr.write(f"{self.author} author used for output\n")
+            seiscomp.logging.debug(f"Filtering origins by author {self.author}")
         except RuntimeError:
             self.author = False
 
@@ -87,14 +172,12 @@ class OriginList(seiscomp.client.Application):
         except RuntimeError:
             self._delimiter = "\n"
 
-        #   sys.stderr.write("Setting end to %s\n" % self._endTime.toString("%F %T"))
-
         return True
 
     def printUsage(self):
         print(
-            """Usage:
-  scorgls [options]
+            f"""Usage:
+  {os.path.basename(__file__)} [options]
 
 List origin IDs available in a given time range and print to stdout."""
         )
@@ -102,13 +185,21 @@ List origin IDs available in a given time range and print to stdout."""
         seiscomp.client.Application.printUsage(self)
 
         print(
-            """Examples:
+            f"""Examples:
 Print all origin IDs from year 2022 and thereafter
-  scorgls -d mysql://sysop:sysop@localhost/seiscomp --begin "2022-01-01 00:00:00"
+  {os.path.basename(__file__)} -d mysql://sysop:sysop@localhost/seiscomp --begin "2022-01-01 00:00:00"
+
+Print IDs of all events in XML file
+  {os.path.basename(__file__)} -i origins.xml
 """
         )
 
     def run(self):
+        if self._inputFile:
+            out = readXML(self)
+            print(f"{self._delimiter.join(out)}\n", file=sys.stdout)
+            return True
+
         seiscomp.logging.debug(f"Search interval: {self._startTime} - {self._endTime}")
         out = []
         q = (
