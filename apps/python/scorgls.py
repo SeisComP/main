6e04a62826ac47e3de0e8f1d9da5955caab8fc06
Dirk Roessler <roessler@gempa.de>
2023-09-09 21:11:55 +0200

    [scorgls.py] Lint by flynt and black

--- apps/python/scorgls.py
+++ apps/python/scorgls.py
@@ -33,17 +33,26 @@ class OriginList(seiscomp.client.Application):
 
     def createCommandLineDescription(self):
         self.commandline().addGroup("Origins")
-        self.commandline().addStringOption("Origins", "begin",
-                                           "The lower bound of the time interval. Format: '1970-01-01 00:00:00'.")
-        self.commandline().addStringOption("Origins", "end",
-                                           "The upper bound of the time interval. Format: '1970-01-01 00:00:00'.")
-        self.commandline().addStringOption("Origins", "author",
-                                           "The author of the origins.")
+        self.commandline().addStringOption(
+            "Origins",
+            "begin",
+            "The lower bound of the time interval. Format: '1970-01-01 00:00:00'.",
+        )
+        self.commandline().addStringOption(
+            "Origins",
+            "end",
+            "The upper bound of the time interval. Format: '1970-01-01 00:00:00'.",
+        )
+        self.commandline().addStringOption(
+            "Origins", "author", "The author of the origins."
+        )
 
         self.commandline().addGroup("Output")
-        self.commandline().addStringOption("Output", "delimiter,D",
-                                           "The delimiter of the resulting "
-                                           "origin IDs. Default: '\\n')")
+        self.commandline().addStringOption(
+            "Output",
+            "delimiter,D",
+            "The delimiter of the resulting " "origin IDs. Default: '\\n')",
+        )
         return True
 
     def init(self):
@@ -53,24 +62,23 @@ class OriginList(seiscomp.client.Application):
         try:
             start = self.commandline().optionString("begin")
             if not self._startTime.fromString(start, "%F %T"):
-                print("Wrong 'begin' given -> assuming {}"
-                      .format(self._startTime), file=sys.stderr)
+                print(
+                    f"Wrong 'begin' given -> assuming {self._startTime}",
+                    file=sys.stderr,
+                )
         except RuntimeError:
-            print("No 'begin' given -> assuming {}".format(self._startTime),
-                  file=sys.stderr)
+            print(f"No 'begin' given -> assuming {self._startTime}", file=sys.stderr)
 
         try:
             end = self.commandline().optionString("end")
             if not self._endTime.fromString(end, "%F %T"):
-                print("Wrong 'end' given -> assuming {}"
-                      .format(self._endTime), file=sys.stderr)
+                print(f"Wrong 'end' given -> assuming {self._endTime}", file=sys.stderr)
         except RuntimeError:
-            print("No 'end' given -> assuming {}".format(self._endTime),
-                  file=sys.stderr)
+            print(f"No 'end' given -> assuming {self._endTime}", file=sys.stderr)
 
         try:
             self.author = self.commandline().optionString("author")
-            sys.stderr.write("%s author used for output\n" % (self.author))
+            sys.stderr.write(f"{self.author} author used for output\n")
         except RuntimeError:
             self.author = False
 
@@ -79,46 +87,55 @@ class OriginList(seiscomp.client.Application):
         except RuntimeError:
             self._delimiter = "\n"
 
-#   sys.stderr.write("Setting end to %s\n" % self._endTime.toString("%F %T"))
+        #   sys.stderr.write("Setting end to %s\n" % self._endTime.toString("%F %T"))
 
         return True
 
     def printUsage(self):
-
-        print('''Usage:
+        print(
+            """Usage:
   scorgls [options]
 
-List origin IDs available in a given time range and print to stdout.''')
+List origin IDs available in a given time range and print to stdout."""
+        )
 
         seiscomp.client.Application.printUsage(self)
 
-        print('''Examples:
+        print(
+            """Examples:
 Print all origin IDs from year 2022 and thereafter
   scorgls -d mysql://sysop:sysop@localhost/seiscomp --begin "2022-01-01 00:00:00"
-''')
+"""
+        )
 
     def run(self):
-        seiscomp.logging.debug("Search interval: %s - %s" %
-                               (self._startTime, self._endTime))
+        seiscomp.logging.debug(f"Search interval: {self._startTime} - {self._endTime}")
         out = []
-        q = "select PublicObject.%s, Origin.* from Origin, PublicObject where Origin._oid=PublicObject._oid and Origin.%s >= '%s' and Origin.%s < '%s'" %\
-            (self.database().convertColumnName("publicID"),
-             self.database().convertColumnName("time_value"),
-             self.database().timeToString(self._startTime),
-             self.database().convertColumnName("time_value"),
-             self.database().timeToString(self._endTime))
+        q = (
+            "select PublicObject.%s, Origin.* from Origin, PublicObject where Origin._oid=PublicObject._oid and Origin.%s >= '%s' and Origin.%s < '%s'"
+            % (
+                self.database().convertColumnName("publicID"),
+                self.database().convertColumnName("time_value"),
+                self.database().timeToString(self._startTime),
+                self.database().convertColumnName("time_value"),
+                self.database().timeToString(self._endTime),
+            )
+        )
 
         if self.author:
-            q += " and Origin.%s = '%s' " %\
-                 (self.database().convertColumnName("creationInfo_author"),
-                  self.query().toString(self.author))
+            q += " and Origin.%s = '%s' " % (
+                self.database().convertColumnName("creationInfo_author"),
+                self.query().toString(self.author),
+            )
 
-        for obj in self.query().getObjectIterator(q, seiscomp.datamodel.Origin.TypeInfo()):
+        for obj in self.query().getObjectIterator(
+            q, seiscomp.datamodel.Origin.TypeInfo()
+        ):
             org = seiscomp.datamodel.Origin.Cast(obj)
             if org:
                 out.append(org.publicID())
 
-        print("{}\n".format(self._delimiter.join(out)), file=sys.stdout)
+        print(f"{self._delimiter.join(out)}\n", file=sys.stdout)
         return True
 
 
