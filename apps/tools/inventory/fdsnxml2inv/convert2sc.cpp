cd019a9abe62339c551e3b5bba4cf398e6fece7f
Jan Becker <jabe@gempa.de>
2024-08-09 13:28:01 +0200

    [fdsnxml2inv] Derive stream sampling rate from decimation stages if not given explicitly

--- apps/tools/inventory/fdsnxml2inv/convert2sc.cpp
+++ apps/tools/inventory/fdsnxml2inv/convert2sc.cpp
@@ -2265,7 +2265,7 @@ bool Convert2SC::process(DataModel::Network *sc_net,
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 bool Convert2SC::process(DataModel::SensorLocation *sc_loc,
-                          const FDSNXML::Channel *cha) {
+                         const FDSNXML::Channel *cha) {
 	bool newInstance = false;
 	bool needUpdate = false;
 
@@ -2459,6 +2459,38 @@ bool Convert2SC::process(DataModel::SensorLocation *sc_loc,
 	sc_stream->setDatalogger(sc_dl->publicID());
 
 	int numerator, denominator;
+
+	try {
+		sc_stream->sampleRateNumerator();
+		sc_stream->sampleRateDenominator();
+	}
+	catch ( ... ) {
+		SEISCOMP_INFO("%s: no sampling rate given, trying to derive from decimations",
+		              chaCode);
+
+		OPT(Fraction) sr;
+
+		for ( const auto stage : stages ) {
+			try {
+				if ( stage->decimation().factor() > 0 ) {
+					double samplingRate = stage->decimation().inputSampleRate() / stage->decimation().factor();
+					sr = double2frac(samplingRate);
+				}
+				else {
+					SEISCOMP_WARNING("%s: stage #%d has an invalid decimation factor",
+					                 chaCode, stage->number());
+				}
+			}
+			catch ( ... ) {}
+		}
+
+		if ( sr ) {
+			SEISCOMP_INFO("%s: derived sampling rate with %d/%d sps",
+			              chaCode, sr->first, sr->second);
+			sc_stream->setSampleRateNumerator(sr->first);
+			sc_stream->setSampleRateDenominator(sr->second);
+		}
+	}
 
 	try {
 		numerator = sc_stream->sampleRateNumerator();
